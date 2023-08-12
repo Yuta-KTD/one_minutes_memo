c104be1d26bab8dc6f8ea619cb3f907db33c3fb4
Yuta-KTD <yuta.katada.2@gmail.com>
2023-08-12 19:00:19 +0900

    GoRouter、Riverpod追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:one_minutes_memo/router/router.dart';
 
-class OneMinutesMemoApp extends StatelessWidget {
+class OneMinutesMemoApp extends ConsumerWidget {
   const OneMinutesMemoApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(routerProvider);
+
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // routerConfigでrouterDelegateやbackButtonDispatcherなどをまとめて設定してくれる
+      // NOTE: 今後ルーティングで課題が出たらrouterDelegateなどを個別設定する
+      routerConfig: goRouter,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(const OneMinutesMemoApp());
+  runApp(
+    const ProviderScope(child: OneMinutesMemoApp()),
+  );
 }
