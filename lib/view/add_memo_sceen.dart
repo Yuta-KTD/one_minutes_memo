0890a024b75923a0ed65b42673b373deec1ec8bb
Yuta-KTD <61367978+Yuta-KTD@users.noreply.github.com>
2023-08-16 23:59:53 +0900

    🐛 : バリデーションエラー→登録→バリデーション成功で正常に値が送れるようにする (#4)
    `FormBuilderValidators`を指定してる方でエラーテキストを登録するように修正
    errorTextプロパティは不要になったので指定しない
    
    ---------
    
    Co-authored-by: katada <katada@ferix.jp>

--- lib/view/add_memo_sceen.dart
+++ lib/view/add_memo_sceen.dart
@@ -18,7 +18,6 @@ class AddMemoScreen extends ConsumerStatefulWidget {
 
 class AddMemoScreenState extends ConsumerState<AddMemoScreen> {
   final _formKey = GlobalKey<FormBuilderState>();
-  String? _memoError;
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +36,12 @@ class AddMemoScreenState extends ConsumerState<AddMemoScreen> {
                   child: PrimaryTextField(
                     name: "memo",
                     labelText: "本文",
-                    errorText: _memoError,
                     isMultiLineInput: true,
-                    validator: FormBuilderValidators.compose(
-                      [FormBuilderValidators.required()],
-                    ),
-                    onChanged: (_) => _onChanged(),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(
+                        errorText: '最低1文字は入力が必要です',
+                      )
+                    ]),
                   ),
                 ),
                 PrimaryButton(
@@ -87,20 +86,8 @@ class AddMemoScreenState extends ConsumerState<AddMemoScreen> {
     );
   }
 
-  void _onChanged() {
-    _formKey.currentState?.save();
-    print('test');
-  }
-
   Future<void> _onPressed() async {
-    setState(() => _memoError = null);
-
-    print(_formKey.currentState!.value["memo"]);
-    // print(_formKey.currentState!.validate());
-
-    // FIXME: 未入力→入力で登録をしてもバリデーションに引っかかる
-    if (!_formKey.currentState!.validate()) {
-      setState(() => _memoError = '最低1文字は入力が必要です');
+    if (!_formKey.currentState!.saveAndValidate()) {
       return;
     }
 
@@ -110,8 +97,6 @@ class AddMemoScreenState extends ConsumerState<AddMemoScreen> {
     ref
         .watch(memoContentsProvider.notifier)
         .addMemo(_formKey.currentState!.value["memo"]);
-
-    // print(_formKey.currentState!.value["memo"]);
 
     _formKey.currentState!.patchValue({'memo': ''});
   }
