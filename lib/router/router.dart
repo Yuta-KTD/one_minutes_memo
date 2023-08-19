5da1fc3b53d97068e2f578085c20561282c19f45
Yuta-KTD <61367978+Yuta-KTD@users.noreply.github.com>
2023-08-19 14:21:05 +0900

    🗑️: 不要な表記を削除 (#7)
    - app.dartのcreate時に生成されるコードを削除
    - ライブラリ以外のimportを相対パスに変更
    - 不要なimportを削除（静的解析強化したい）

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:one_minutes_memo/view/add_memo_sceen.dart';
-import 'package:one_minutes_memo/view/sign_in_screen.dart';
-import 'package:one_minutes_memo/view/sign_up_screen.dart';
-import 'package:one_minutes_memo/view/title_screen.dart';
+
+import '../view/add_memo_sceen.dart';
+import '../view/sign_in_screen.dart';
+import '../view/sign_up_screen.dart';
+import '../view/title_screen.dart';
 
 final routerProvider = Provider(
   (ref) => GoRouter(
