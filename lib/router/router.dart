19191e5da559159d02dcc99d71f6062264c6e960
Yuta-KTD <yuta.katada.2@gmail.com>
2023-10-01 18:53:45 +0900

    やりたい構成に合わせる

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -2,9 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:one_minutes_memo/feature/auth/presentation/sign_up/sign_up_screen.dart';
 
-import '../feature/auth/view/add_memo_sceen.dart';
 import '../feature/auth/view/sign_in_screen.dart';
-import '../feature/auth/view/title_screen.dart';
+import '../feature/memo/presentation/add_memo_sceen.dart';
+import '../feature/memo/presentation/title_screen.dart';
 
 final routerProvider = Provider(
   (ref) => GoRouter(
