0efb889f49077daa6e39c65cd5d8f39504b64087
Yuta-KTD <yuta.katada.2@gmail.com>
2023-10-02 22:44:36 +0900

    ログイン状態で遷移先を変える

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:one_minutes_memo/feature/auth/presentation/sign_up/sign_up_screen.dart';
+import 'package:one_minutes_memo/feature/auth/data/repository/auth_repository_impl.dart';
+import 'package:one_minutes_memo/feature/auth/presentation/sign_up_screen.dart';
+import 'package:one_minutes_memo/feature/auth/view/sign_in_screen.dart';
+import 'package:one_minutes_memo/feature/memo/presentation/add_memo_sceen.dart';
+import 'package:one_minutes_memo/feature/memo/presentation/title_screen.dart';
 
-import '../feature/auth/view/sign_in_screen.dart';
-import '../feature/memo/presentation/add_memo_sceen.dart';
-import '../feature/memo/presentation/title_screen.dart';
-
-final routerProvider = Provider(
+final routerProvider = Provider.autoDispose<GoRouter>(
   (ref) => GoRouter(
     routes: [
       GoRoute(
@@ -28,5 +28,12 @@ final routerProvider = Provider(
         ],
       ),
     ],
+    redirect: (context, state) {
+      final authState = ref.watch(authStateChangesProvier).valueOrNull;
+      if (authState != null) {
+        return '/title';
+      }
+      return '/';
+    },
   ),
 );
